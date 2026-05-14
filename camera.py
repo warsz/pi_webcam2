eb0f7abe13c153db7a99477e5cac1b5cbdee0836
Jakub Warszawski <jakub.warszawski@knowit.no>
2026-05-14 09:50:30 +0200

    Add first draft of meter_light function

--- camera.py
+++ camera.py
@@ -1,6 +1,7 @@
 import tomllib
 from picamera2 import Picamera2
 from libcamera import Transform
+import numpy as np
 
 TRANSFORMS = {
     "hvflip":   Transform(vflip=True, hflip=True),
@@ -13,10 +14,35 @@ def load_config(path="config.toml"):
     with open(path, "rb") as f:
         return tomllib.load(f)
 
+def meter_light(cfg: dict):
+    
+    
+    # start camera
+    cam = Picamera2()
+    config = cam.create_still_configuration(
+        raw={"size": tuple(cfg["camera"]["raw_resolution"])},
+        main={"size": tuple(cfg["camera"]["resolution"])},
+        transform=TRANSFORMS[cfg["camera"]["rotation"]]
+    )
+    cam.configure(config)
+    cam.start()
+    # capture_array() -> numpy array
+    arr = cam.capture_array()
+    # slice the light_region patch
+    x1, y1, x2, y2 = cfg["metering"]["light_region"]
+    patch = arr[y1:y2, x1:x2]
+    # np.mean() -> return the float
+    intensity = np.mean(patch)
+    # close camera
+    cam.close()
+    return intensity
+    
+
 def main():
     cfg = load_config()
     
     rotation = cfg["camera"]["rotation"]
+    raw =cfg["camera"]["raw_resolution"]
     resolution = tuple(cfg["camera"]["resolution"])
     latest_path = cfg["image"]["latest_path"]
 
@@ -32,6 +58,8 @@ def main():
     cam.start()
     cam.capture_file(latest_path) 
     cam.close()
+
+    print(meter_light(cfg))
 
 if __name__ == "__main__":
     main()
